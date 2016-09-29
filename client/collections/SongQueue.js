17a6bf435a5d88dc75313e27095e3b300ab19892
archcorsair <archcorsair@gmail.com>
2016-09-28 17:29:33 -0700

    Specs passing for: SongModel, SongQueue, SongQueueView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add enqueue', function () {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function (song) {
+      this.shift();
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function (song) {
+      // remove the song from the queue
+      this.remove(song);
+    });
+  },
+
+  playFirst: function () {
+    this.first().play();
   }
 
 });
