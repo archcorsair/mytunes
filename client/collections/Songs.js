6ef155f89d7b733ba224d0d01ef92bf0289365b7
archcorsair <archcorsair@gmail.com>
2016-09-29 11:03:57 -0700

    Pass remaining pending Songs tests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,24 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    var _this = this;
+    console.log('outside: ', this);
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.parse.com/1/classes/songs/',
+      success: function(someD) {
+        console.log('AJAX Success');
+        console.log('inside: ', _this);
+        someD.results.forEach(function(song) {
+          _this.add(song);
+        });
+        _this.trigger('loaded');
+      }
+    });
+
+  }
 
 });
